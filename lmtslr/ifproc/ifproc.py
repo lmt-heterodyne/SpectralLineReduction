5ac0c359020ca6082295d38c7bd9d6c83e3708c0
Peter Teuben <teuben@gmail.com>
2022-02-21 14:48:16 -0500

    restore what seems to was a formatting bug in commented code

--- lmtslr/ifproc/ifproc.py
+++ lmtslr/ifproc/ifproc.py
@@ -38,7 +38,8 @@ def lookup_ifproc_file(obsnum,path='/data_lmt/ifproc/'):
             print('found %s'%(file))
             filename = path+file
     if filename == '':
-        print('lookup_ifproc_file: no file for obsnum ', obsnum)        if 'lmttpm' not in path:
+        print('lookup_ifproc_file: no file for obsnum ', obsnum)
+        if 'lmttpm' not in path:
             print('look in lmttpm')
             return lookup_ifproc_file(obsnum,path='/data_lmt/lmttpm/')
     return(filename)
