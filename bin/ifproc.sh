16681bf329734405198149cf8d14e8c39a9c27aa
Peter Teuben <teuben@gmail.com>
2022-03-14 05:27:30 -0400

    new: create ifproc header

---
+++ bin/ifproc.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+#
+
+obsnum=$1
+
+if [ -z "$obsnum" ]; then
+    echo usage: $0 obsnum
+    echo Reports the ifproc Header variables
+    exit 0
+fi
+
+tmp=/tmp/tmp$$.rc
+
+ifproc="obsnum $obsnum"
+lmtinfo.py $obsnum | grep '# ifproc' | cut -c3- > $tmp.rc
+source $tmp.rc
+rm $tmp.rc
+
+if [ -e "$ifproc" ]; then
+    echo "$ifproc"
+    ncdump -l 256 $ifproc | grep '^ Header' | column -s= -t 
+else
+    echo $ifproc does not exist
+fi
